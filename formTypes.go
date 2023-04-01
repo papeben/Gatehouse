483e89e6f470012c9caff8c1625f4055ea229ee6
--global <--global>
2023-04-01 12:39:57 +0100

    Modular code-defined forms

---
+++ formTypes.go
@@ -0,0 +1,115 @@
+package main
+
+type GatehouseForm struct {
+	TabTitle     string
+	FormTitle    string
+	FormAction   string
+	FormMethod   string
+	FormElements []GatehouseFormElement
+	OIDCButtons  bool
+	OIDCOptions  []OIDCButton
+}
+
+type GatehouseFormElement struct {
+	Class            string
+	InnerText        string
+	IsLink           bool
+	LinkURI          string
+	IsInput          bool
+	InputType        string
+	InputName        string
+	InputPlaceholder string
+}
+
+type OIDCButton struct {
+	Text            string
+	ImageURI        string
+	BackgroundColor string
+	TextColor       string
+	URI             string
+}
+
+func FormCreateTextInput(name string, placeholder string) GatehouseFormElement {
+	return GatehouseFormElement{
+		"gh_inp_text",
+		"",
+		false,
+		"",
+		true,
+		"text",
+		name,
+		placeholder,
+	}
+}
+func FormCreatePasswordInput(name string, placeholder string) GatehouseFormElement {
+	return GatehouseFormElement{
+		"gh_inp_text",
+		"",
+		false,
+		"",
+		true,
+		"password",
+		name,
+		placeholder,
+	}
+}
+func FormCreateSubmitInput(name string, text string) GatehouseFormElement {
+	return GatehouseFormElement{
+		"gh_inp_button",
+		text,
+		false,
+		"",
+		true,
+		"submit",
+		name,
+		"",
+	}
+}
+func FormCreateButtonLink(linkUrl string, text string) GatehouseFormElement {
+	return GatehouseFormElement{
+		"gh_a_button",
+		text,
+		true,
+		linkUrl,
+		false,
+		"",
+		"",
+		"",
+	}
+}
+func FormCreateSmallLink(linkUrl string, text string) GatehouseFormElement {
+	return GatehouseFormElement{
+		"gh_a_small",
+		text,
+		true,
+		linkUrl,
+		false,
+		"",
+		"",
+		"",
+	}
+}
+func FormCreateDivider() GatehouseFormElement {
+	return GatehouseFormElement{
+		"gh_div_divider",
+		"",
+		false,
+		"",
+		false,
+		"",
+		"",
+		"",
+	}
+}
+func FormCreateHint(text string) GatehouseFormElement {
+	return GatehouseFormElement{
+		"gh_div_hint",
+		text,
+		false,
+		"",
+		false,
+		"",
+		"",
+		"",
+	}
+}
--- main.go
+++ main.go
@@ -1,31 +1,98 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"strings"
 )
 
 func main() {
-	// Load configuration from environment or set defaults
-	backendServerAddr := envWithDefault("BACKEND_SERVER", "127.0.0.1")
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// LOAD ENVIRONMENT VARIABLES
+	backendServerAddr := envWithDefault("BACKEND_SERVER", "127.0.0.1") // Load configuration from environment or set defaults
 	backendServerPort := envWithDefault("BACKEND_PORT", "9000")
 	listenPort := envWithDefault("LISTEN_PORT", "8080")
+	functionalPath := envWithDefault("GATEHOUSE_PATH", "gatehouse")
+	appName := envWithDefault("APP_NAME", "Gatehouse")
 
-	// Validate URL
-	url, err := url.Parse("http://" + backendServerAddr + ":" + backendServerPort)
+	functionalURIs := map[string]map[string]string{
+		"GET": {
+			"/" + functionalPath + "/login":    "login",
+			"/" + functionalPath + "/register": "register",
+		},
+	}
+	url, err := url.Parse("http://" + backendServerAddr + ":" + backendServerPort) // Validate backend URL
 	if err != nil {
 		panic(err)
 	}
-	proxy := httputil.NewSingleHostReverseProxy(url)
 
-	// Create main listener function
-	http.HandleFunc("/", func(response http.ResponseWriter, request *http.Request) {
-		fmt.Println(request.URL.Path)
-		proxy.ServeHTTP(response, request)
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// ASSEMBLE FORM PAGES
+	formTemplate, err := template.ParseFiles("template/form.html")
+	if err != nil {
+		panic(err)
+	}
+
+	loginPage := GatehouseForm{ // Define login page
+		appName + " - Sign in",
+		"Sign In",
+		"/submit",
+		"POST",
+		[]GatehouseFormElement{
+			FormCreateDivider(),
+			FormCreateTextInput("username", "Username"),
+			FormCreatePasswordInput("password", "Password"),
+			FormCreateSmallLink("/"+functionalPath+"/forgot", "Forgot my password..."),
+			FormCreateSubmitInput("signin", "Sign In"),
+			FormCreateDivider(),
+			FormCreateButtonLink("/"+functionalPath+"/register", "Create an Account"),
+			FormCreateDivider(),
+		},
+		false,
+		[]OIDCButton{},
+	}
+
+	registrationPage := GatehouseForm{ // Define login page
+		appName + " - Create Account",
+		"Create an Account",
+		"/submit",
+		"POST",
+		[]GatehouseFormElement{
+			FormCreateDivider(),
+			FormCreateTextInput("username", "Username"),
+			FormCreateTextInput("email", "Email Address"),
+			FormCreatePasswordInput("password", "Password"),
+			FormCreatePasswordInput("passwordConfirm", "Confirm Password"),
+			FormCreateSubmitInput("register", "Create Account"),
+			FormCreateDivider(),
+			FormCreateHint("Already have an account?"),
+			FormCreateButtonLink("/"+functionalPath+"/login", "Sign In"),
+			FormCreateDivider(),
+		},
+		false,
+		[]OIDCButton{},
+	}
+
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// MAIN REQUEST HANDLER
+	proxy := httputil.NewSingleHostReverseProxy(url)
+	http.HandleFunc("/", func(response http.ResponseWriter, request *http.Request) { // Create main listener function
+		gateFunction := functionalURIs[request.Method][strings.ToLower(request.URL.Path)] // Load action associated with URI from functionalURIs map
+
+		if gateFunction != "" { // If functional URL
+			switch gateFunction { // Serve appropriate page
+			case "login":
+				formTemplate.Execute(response, loginPage)
+			case "register":
+				formTemplate.Execute(response, registrationPage)
+			}
+		} else {
+			proxy.ServeHTTP(response, request)
+		}
 	})
 
 	log.Fatal(http.ListenAndServe(":"+listenPort, nil))
