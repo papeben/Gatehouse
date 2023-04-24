0e3bc7a583ace53a24b49c15f54998d105891bba
Ben Pape <benjaminpape@protonmail.com>
2023-04-24 16:43:18 +0100

    OTP crypto functions

--- crypto.go
+++ crypto.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"crypto/hmac"
 	"crypto/rand"
+	"crypto/sha1"
+	"encoding/base32"
+	"encoding/binary"
+	"fmt"
 	"math/big"
+	"time"
 
 	"golang.org/x/crypto/bcrypt"
 )
@@ -52,4 +58,46 @@ func GenerateRandomNumbers(length int) string {
 	}
 
 	return string(bytes)
+}
+
+func GenerateOTP(secret string, timeStep int64) (string, error) {
+	// Decode the secret key from base32
+	key, err := base32.StdEncoding.DecodeString(secret)
+	if err != nil {
+		return "", fmt.Errorf("failed to decode secret key: %v", err)
+	}
+
+	// Calculate the number of time steps that have elapsed since the Unix epoch
+	now := time.Now().Unix()
+	timeStepCount := uint64(now / timeStep)
+
+	// Convert the time step count to a byte array in big-endian order
+	counterBytes := make([]byte, 8)
+	binary.BigEndian.PutUint64(counterBytes, timeStepCount)
+
+	// Generate an HMAC-SHA1 hash using the secret key and time step count
+	hash := hmac.New(sha1.New, key)
+	hash.Write(counterBytes)
+	sum := hash.Sum(nil)
+
+	// Calculate the offset into the hash to use as the starting point for the OTP
+	offset := sum[len(sum)-1] & 0x0f
+
+	// Extract a 4-byte integer from the hash starting at the given offset
+	value := binary.BigEndian.Uint32(sum[offset:])
+
+	// Truncate the integer to a 6-digit value and return it as a string
+	otp := fmt.Sprintf("%06d", value%1000000)
+	return otp, nil
+}
+
+func GenerateOTPSecret() string {
+	// Generate a random secret key
+	secret := make([]byte, 10)
+	_, err := rand.Read(secret)
+	if err != nil {
+		panic(err)
+	}
+	secretBase32 := base32.StdEncoding.EncodeToString(secret)
+	return secretBase32
 }
