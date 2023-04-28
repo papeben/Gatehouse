4e7456a540fe98628785e3fb140cc3803464fc35
Ben Pape <benjaminpape@protonmail.com>
2023-04-28 16:56:03 +0100

    Reset submission tidy

--- handlers.go
+++ handlers.go
@@ -324,10 +324,33 @@ func HandleSubResetRequest(response http.ResponseWriter, request *http.Request) 
 }
 
 func HandleSubReset(response http.ResponseWriter, request *http.Request) {
-	if ResetSubmission(request) {
-		err := formTemplate.Execute(response, resetSuccessPage)
+	code := request.URL.Query().Get("c")
+	password := request.FormValue("password")
+	passwordConfirm := request.FormValue("passwordConfirm")
+
+	if code != "" && IsValidPassword(password) && password == passwordConfirm {
+		db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", mysqlUser, mysqlPassword, mysqlHost, mysqlPort, mysqlDatabase))
 		if err != nil {
 			panic(err)
+		}
+		defer db.Close()
+
+		var userID string
+		err = db.QueryRow(fmt.Sprintf("SELECT user_id FROM %s_resets WHERE reset_token = ? AND used = 0", tablePrefix), code).Scan(&userID)
+		if err == sql.ErrNoRows {
+			response.WriteHeader(403)
+			fmt.Fprint(response, `403 - Unauthorized.`)
+		} else if err != nil {
+			panic(err)
+		} else {
+			_, err = db.Exec(fmt.Sprintf("UPDATE %s_resets INNER JOIN %s_accounts ON user_id = id SET password = ?, used = 1 WHERE reset_token = ?", tablePrefix, tablePrefix), HashPassword(password), code)
+			if err != nil {
+				panic(err)
+			}
+			err := formTemplate.Execute(response, resetSuccessPage)
+			if err != nil {
+				panic(err)
+			}
 		}
 	} else {
 		response.WriteHeader(400)
